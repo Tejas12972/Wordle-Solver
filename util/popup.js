b490f73ffc23fdc46f0c61bc2f606fa7595e65ed
Tejas <77419101+Tejas12972@users.noreply.github.com>
2024-06-18 13:10:25 -0400

    update

--- util/popup.js
+++ util/popup.js
@@ -1,8 +1,12 @@
-    let answ = runScript();
-    document.getElementById("answer_xfg").innerText = answ
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('solve').addEventListener('click', solveWordle);
+});
 
-    function runScript() {
-        chrome.tabs.executeScript({
-            file: './solver.js'
-        })
-    }
+const solveWordle = () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            files: ['util/solver.js']
+        });
+    });
+};
